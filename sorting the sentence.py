958c96854a0118bcf52f3d3b8765e84e30d7161b
alankrutha2007 <alankruthagade@gmail.com>
2025-11-03 20:05:52 +0530

    Create sorting the sentence.py

---
+++ sorting the sentence.py
@@ -0,0 +1,7 @@
+class Solution:
+    def sortSentence(self, s: str) -> str:
+        words = s.split()
+        words.sort(key=lambda x: int(x[-1]))
+        return ' '.join(word[:-1] for word in words)
+
+        
