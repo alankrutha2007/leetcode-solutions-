45d0e7a2ba2805e0574bf2b16b5e1c548fcac455
alankrutha2007 <alankruthagade@gmail.com>
2025-11-16 17:47:45 +0530

    Create Binary search.py

---
+++ Binary search.py
@@ -0,0 +1,16 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        def binary_search(nums,target):
+            low = 0 
+            high = len(nums)-1 
+            while(low<=high):
+                mid = (low+high)//2 
+                if(nums[mid]==target):
+                    return mid
+                elif(target>nums[mid]):
+                    low = mid+1 
+                elif(target<nums[mid]):
+                    high = mid-1 
+            return -1 
+        return(binary_search(nums,target)) 
+                
