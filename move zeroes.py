910d7fe7272876c20383bba31d943cab440ea27e
alankrutha2007 <alankruthagade@gmail.com>
2025-11-28 21:03:28 +0530

    Create move zeroes.py

---
+++ move zeroes.py
@@ -0,0 +1,12 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        p=0
+        for i in range(len(nums)):
+            if nums[i]!=0:
+                nums[p] = nums[i]
+                p+=1
+        for i in range(p,len(nums)):
+            nums[i]=0
+        return nums
+
+        
