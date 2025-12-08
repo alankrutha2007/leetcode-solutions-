6ddcbde6ec4b9dc438a99215b3d22939c4d14cc7
alankrutha2007 <alankruthagade@gmail.com>
2025-12-08 23:39:19 +0530

    Create Count square sum triples.py

---
+++ Count square sum triples.py
@@ -0,0 +1,14 @@
+class Solution:
+    def countTriples(self, n: int) -> int:
+        import math
+        count=0
+        for a in range(1,n+1):
+            for b in range(1,n+1):
+                c_square=a*a+b*b
+                c=int(math.isqrt(c_square))
+                if(c*c==c_square and c<=n):
+                    count+=1
+        return count
+
+
+        
