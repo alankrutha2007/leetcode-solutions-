c06795d8a30030456d083635ef133a8139c26c97
alankrutha2007 <alankruthagade@gmail.com>
2025-12-10 23:17:21 +0530

    Create Ugly number.py

---
+++ Ugly number.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isUgly(self, n: int) -> bool:
+        if(n<=0):
+            return False
+        x=[2,3,5]
+        for i in x:
+            while(n%i==0):
+                n=n//i
+        return n==1
+        
