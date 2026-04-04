42563b33bc90e0d4df917743e4d19cb1a482f5f8
alankrutha2007 <alankruthagade@gmail.com>
2026-04-04 18:36:19 +0530

    Create Ransom Note.py

---
+++ Ransom Note.py
@@ -0,0 +1,6 @@
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        for ch in ransomNote:
+            if(ransomNote.count(ch)>magazine.count(ch)):
+                return False
+        return True  
