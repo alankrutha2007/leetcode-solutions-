77af5ff49254fbc68a917332063e7782ee259a2a
alankrutha2007 <alankruthagade@gmail.com>
2026-06-21 21:02:12 +0530

    Create Count Common Words With One Occurences.py

---
+++ Count Common Words With One Occurences.py
@@ -0,0 +1,10 @@
+from collections import Counter
+class Solution:
+    def countWords(self, words1: List[str], words2: List[str]) -> int:
+        count1=Counter(words1)
+        count2=Counter(words2)
+        ans=0
+        for word in count1:
+            if count1[word]==1 and count2[word]==1:
+                ans+=1
+        return ans
