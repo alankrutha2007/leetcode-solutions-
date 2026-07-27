a7da20dc68d495abb16a2d1a44ec55f15fc0b45b
alankrutha2007 <alankruthagade@gmail.com>
2026-07-27 19:27:14 +0530

    Create Make Three Strings Equal.py

---
+++ Make Three Strings Equal.py
@@ -0,0 +1,11 @@
+class Solution:
+    def findMinimumOperations(self, s1: str, s2: str, s3: str) -> int:
+        i = 0
+        while i < len(s1) and i < len(s2) and i < len(s3):
+            if s1[i] == s2[i] == s3[i]:
+                i += 1
+            else:
+                break
+        if i == 0:
+            return -1
+        return (len(s1) - i) + (len(s2) - i) + (len(s3) - i)
